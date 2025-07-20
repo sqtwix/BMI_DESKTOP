651ec58e58c9a656a37864e72d083141728edecc
sqtwix <ivan20140767@gmail.com>
2025-07-20 20:25:27 +0300

    Initialize LoginPage, RegPage, MainPage

---
+++ BMI/BMI_client/MainNav.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Navigation;
+
+namespace BMI_client
+{
+    public partial class MainNav : NavigationWindow
+    {
+        public MainNav()
+        {
+            InitializeComponent();  // эта строка вызывает ошибку, если XAML не скомпилировался
+        }
+    }
+}
