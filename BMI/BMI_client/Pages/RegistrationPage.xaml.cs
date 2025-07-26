ce70846b2c3285e81abfd3d8e2783fa0e6eefb49
sqtwix <ivan20140767@gmail.com>
2025-07-26 13:44:03 +0300

    API for registration in WPF project

--- BMI/BMI_client/Pages/RegistrationPage.xaml.cs
+++ BMI/BMI_client/Pages/RegistrationPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace BMI_client.Pages
 {
@@ -25,9 +28,57 @@ namespace BMI_client.Pages
             InitializeComponent();
         }
 
-        private void ToAutorisationPage(object sender, RoutedEventArgs e) // To To AutorisationPage
+        private void ToAutorisationPage(object sender, RoutedEventArgs e) // To AutorisationPage
         {
             NavigationService.Navigate(new LoginPage());
+        }
+
+        private async void RegistrationUser(object sender, RoutedEventArgs e)
+        {
+            await RegisterUser();
+        }
+
+        public async Task RegisterUser() // Api to make new user
+        {
+            var httpClient = new HttpClient();
+            var url = "http://127.0.0.1:8080/user_api/register/";
+
+            string username = Username.Text;
+            string password = Password.Text;
+
+            if (username == "" || username == " " || password == "" || password == " ")
+            {
+                ErrorLabel.Content = "Заполните все поля!!!";
+            }
+            else if (password.Length < 5)
+            {
+                ErrorLabel.Content = "Пароль должен быть минимум из 5 символов!!!";
+            }
+            else
+            {
+                var requestBody = new
+                {
+                    username = username,
+                    password = password,
+                };
+
+                string json = JsonConvert.SerializeObject(requestBody);
+
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var responce = await httpClient.PostAsync(url, content);
+
+                if (responce.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Пользователь зарегестрирован");
+                }
+                else
+                {
+                    string error = await responce.Content.ReadAsStringAsync();
+                    MessageBox.Show("Ошибка регистрации: " + error);
+                }
+
+            }
 
         }
     }
